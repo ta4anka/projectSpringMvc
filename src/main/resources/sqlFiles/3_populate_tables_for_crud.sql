74c09fdf7faabfbd748c4e38bb401bf1dba0b668
Vladymir <vladymirsheyko@gmail.com>
2019-07-01 19:48:48 +0300

    added some changes for heroku

--- src/main/resources/sqlFiles/3_populate_tables_for_crud.sql
+++ src/main/resources/sqlFiles/3_populate_tables_for_crud.sql
@@ -1,22 +1,22 @@
 -- USE employeemanagerDB;
 
-INSERT INTO departments(name)
+INSERT INTO departments(id,name)
 VALUES
-('HR department'),
-('IT department'),
-('R&D department'),
-('Finance department');
+(1,'HR department'),
+(2,'IT department'),
+(3,'R&D department'),
+(4,'Finance department');
 
-INSERT INTO employees(first_name,last_name,salary,birth_day,employment_day,department_id)
+INSERT INTO employees(id,first_name,last_name,salary,birth_day,employment_day,department_id)
 VALUES
-('Brian','Kernighan',7000,'1985-09-15','2009-02-20',11),
-('Dennis','Ritchie',6500,'1984-07-10','2012-04-21',21),
-('Bjarne','Stroustrup',7500,'1980-06-12','2012-07-25',31),
-('James','Gosling',9500,'1979-05-19','2016-01-06',21),
-('Steve','Wozniak',9500,'1982-03-21','2018-09-17',21),
-('Linus','Torvalds',8000,'1987-05-09','2014-08-01',31),
-('Guido','vanRossum',9000,'1988-09-09','2013-09-07',21),
-('Brendan','Eich',6000,'1995-03-05','2019-02-10',31),
-('Brad','Cox',2000,'1999-01-01','2019-05-01',11);
+(1,'Brian','Kernighan',7000,'1985-09-15','2009-02-20',1),
+(2,'Dennis','Ritchie',6500,'1984-07-10','2012-04-21',2),
+(3,'Bjarne','Stroustrup',7500,'1980-06-12','2012-07-25',3),
+(4,'James','Gosling',9500,'1979-05-19','2016-01-06',2),
+(5,'Steve','Wozniak',9500,'1982-03-21','2018-09-17',4),
+(6,'Linus','Torvalds',8000,'1987-05-09','2014-08-01',3),
+(7,'Guido','vanRossum',9000,'1988-09-09','2013-09-07',1),
+(8,'Brendan','Eich',6000,'1995-03-05','2019-02-10',1),
+(9,'Brad','Cox',2000,'1999-01-01','2019-05-01',4);
 
 
--- src/main/resources/sqlFiles/5_populate_tables_for_security.sql
+++ src/main/resources/sqlFiles/5_populate_tables_for_security.sql
@@ -1,18 +1,18 @@
 -- USE employeemanagerDB;
 
 
-INSERT INTO roles(name)
+INSERT INTO roles(id,name)
 VALUES 
-('ROLE_ADMIN'),
-('ROLE_MODERATOR'),
-('ROLE_USER');
+(1,'ROLE_ADMIN'),
+(2,'ROLE_MODERATOR'),
+(3,'ROLE_USER');
 
 
-INSERT INTO users(username,password,role_id)
+INSERT INTO users(id,username,password,role_id)
 VALUES
-('Vladimir','$2a$10$y71Rwd.9kmqbQRY3cdpSweAK4f4FVgsCgvHdlW9y58AQ7w4K7JQM2',1),
-('Vlad','$2a$10$jl7.xjwvQryUcO2SUzvugORf8eJx6HywhTOch9SGSptP5ax8mjTz.',11),
-('Maya','$2a$10$rAIYcBIbzRQrJ5nVwK0iP.PUwI29q1mEIw32Ga9.tIFP6i4arTQ9O',21);
+(1,'Vladimir','$2a$10$y71Rwd.9kmqbQRY3cdpSweAK4f4FVgsCgvHdlW9y58AQ7w4K7JQM2',1),
+(2,'Vlad','$2a$10$jl7.xjwvQryUcO2SUzvugORf8eJx6HywhTOch9SGSptP5ax8mjTz.',2),
+(3,'Maya','$2a$10$rAIYcBIbzRQrJ5nVwK0iP.PUwI29q1mEIw32Ga9.tIFP6i4arTQ9O',3);
 
 
 
